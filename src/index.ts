fad31cb6fa6985fd8aafedc25dacc5cc913399e2
luncheon <takeuck+github@gmail.com>
2017-10-23 00:03:09 +0900

    various fixes

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ export type TimingFunction = 'ease' | 'ease-in' | 'ease-out' | 'ease-in-out' | '
 export interface RippletOptions {
   className:                string | null
   color:                    string | null
-  opacity:                  string | number | null
+  opacity:                  string | null
   spreadingDuration:        string | null
   spreadingDelay:           string | null
   spreadingTimingFunction:  TimingFunction
@@ -15,7 +15,7 @@ export interface RippletOptions {
 export const defaultOptions: RippletOptions = {
   className:                'ripplet',
   color:                    'rgba(0, 0, 0, .1)',
-  opacity:                  1,
+  opacity:                  '1',
   spreadingDuration:        '.4s',
   spreadingDelay:           '0s',
   spreadingTimingFunction:  'ease-out',
@@ -41,32 +41,38 @@ function generateRipplet({ currentTarget: target, clientX, clientY }: { currentT
     containerStyle.top            = `${targetRect.top  + document.documentElement.scrollTop  + document.body.scrollTop}px`
     containerStyle.width          = `${targetRect.width}px`
     containerStyle.height         = `${targetRect.height}px`
-    containerStyle.zIndex         = String((targetStyle.zIndex && parseInt(targetStyle.zIndex, 10) || 0) + 1)
+    containerStyle.zIndex         = `${(targetStyle.zIndex && parseInt(targetStyle.zIndex, 10) || 0) + 1}`
     containerStyle.borderTopLeftRadius      = targetStyle.borderTopLeftRadius
     containerStyle.borderTopRightRadius     = targetStyle.borderTopRightRadius
     containerStyle.borderBottomLeftRadius   = targetStyle.borderBottomLeftRadius
     containerStyle.borderBottomRightRadius  = targetStyle.borderBottomRightRadius
-    containerStyle.transition     = `opacity ${options.clearingDuration} ${options.clearingTimingFunction} ${options.clearingDelay}`
-    containerStyle.opacity        = '1'
+    containerStyle.transitionProperty       = 'opacity'
+    containerStyle.transitionDuration       = options.clearingDuration
+    containerStyle.transitionTimingFunction = options.clearingTimingFunction
+    containerStyle.transitionDelay          = options.clearingDelay
+    containerStyle.opacity                  = '1'
     setTimeout(() => containerStyle.opacity = '0')
   }
 
   const rippletElement = containerElement.appendChild(document.createElement('div'))
-  options.className && (rippletElement.className = options.className)
+  rippletElement.className = options.className || ''
   {
     const distanceX = Math.max(clientX - targetRect.left, targetRect.right - clientX)
     const distanceY = Math.max(clientY - targetRect.top, targetRect.bottom - clientY)
     const radius    = Math.sqrt(distanceX ** 2 + distanceY ** 2)
     const rippletStyle = rippletElement.style
-    options.color   && (rippletStyle.backgroundColor = options.color)
-    options.opacity && (rippletStyle.opacity = String(options.opacity))
-    rippletStyle.width         = `${radius * 2}px`
-    rippletStyle.height        = `${radius * 2}px`
-    rippletStyle.marginLeft    = `${clientX - targetRect.left - radius}px`
-    rippletStyle.marginTop     = `${clientY - targetRect.top  - radius}px`
-    rippletStyle.borderRadius  = '50%'
-    rippletStyle.transition    = `transform ${options.spreadingDuration} ${options.spreadingTimingFunction} ${options.spreadingDelay}`
-    rippletStyle.transform     = 'scale(0)'
+    rippletStyle.backgroundColor            = options.color
+    rippletStyle.opacity                    = options.opacity
+    rippletStyle.width                      = `${radius * 2}px`
+    rippletStyle.height                     = `${radius * 2}px`
+    rippletStyle.marginLeft                 = `${clientX - targetRect.left - radius}px`
+    rippletStyle.marginTop                  = `${clientY - targetRect.top  - radius}px`
+    rippletStyle.borderRadius               = '50%'
+    rippletStyle.transitionProperty         = 'transform'
+    rippletStyle.transitionDuration         = options.spreadingDuration
+    rippletStyle.transitionTimingFunction   = options.spreadingTimingFunction
+    rippletStyle.transitionDelay            = options.spreadingDelay
+    rippletStyle.transform                  = 'scale(0)'
     setTimeout(() => rippletStyle.transform = 'scale(1)')
   }
 
